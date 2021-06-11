496cba0c2578f625abcb54bc397c2b5b0ec831b4
Evgenya <evgenyakorolkova@mail.ru>
2021-06-11 16:44:03 +0400

    added unit project

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        dots: false,
+        navSpeed: 1000
+        // navText: ['', '']
+    });
+  });
